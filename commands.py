ea868e12992dbfe9bbc7562b368f8c6c8c813c16
Chandan Relekar <61446958+chanrt@users.noreply.github.com>
2021-11-02 18:22:35 +0530

    Implemented commands

---
+++ commands.py
@@ -0,0 +1,41 @@
+import os, sys
+from variables import vars
+
+def about():
+    print("\nVerbose is intended to be a dynamically typed, completely stack-based and easily readable programming language")
+    print("Developed by: chanrt | Fork me at GitHub!")
+    print("Version: alpha\n")
+
+class Command:
+    def __init__(self, identifiers, function):
+        self.identifiers = identifiers
+        self.function = function
+
+class Commands:
+    def __init__(self):
+        self.definitions = []
+
+    def add(self, string, function):
+        new_command = Command(string, function)
+        self.definitions.append(new_command)
+
+    def check(self, string):
+        for command in self.definitions:
+            if string in command.identifiers:
+                return True
+        return False
+
+    def execute(self, string):
+        for command in self.definitions:
+            if string in command.identifiers:
+                command.function()
+
+CommandManager = Commands()
+
+CommandManager.add(["@quit", "@exit"], quit)
+CommandManager.add(["@vars"], vars.printStack)
+CommandManager.add(["@about"], about)
+
+
+
+
--- shell.py
+++ shell.py
@@ -1,3 +1,4 @@
+from commands import CommandManager
 from lexical_analyser import Analyser
 from evaluator import Evaluator
 
@@ -7,13 +8,13 @@ if __name__ == "__main__":
     
     print("--> Verbose Interpreter <--\n")
     print("Repository at https://github.com/chanrt/verbose-interpreter-python")
-    print("Enter quit() to exit, or just close the window\n")
+    print("Enter @quit to exit, or just close the window\n")
 
     while True:
         input_string = input("> ")
         
-        if input_string == "quit()":
-            break
+        if CommandManager.check(input_string):
+            CommandManager.execute(input_string)
         else:
             analyser = Analyser(input_string, debug = debug_status)
             evaluator = Evaluator(analyser.line_stack, echo = True, debug = debug_status)
